e9dd88ccb66c1900c8843866cb828dce137d7bf3
ssihuiong <ongsihui01@gmail.com>
2023-10-31 20:18:08 +0800

    updated encoder pins

--- driver/encoder/encoder.c
+++ driver/encoder/encoder.c
@@ -7,14 +7,17 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
+#include "motor.h"
 
 // Define GP2 and GP3 for the left and right encoders
-#define LEFT_ENCODER_PIN 2
-#define RIGHT_ENCODER_PIN 3
+#define LEFT_ENCODER_PIN 21
+#define LEFT_VCC 22
+#define RIGHT_ENCODER_PIN 8
+#define RIGHT_VCC 9
 
 // Constants to calculate distance in centimeters
 const uint32_t ENCODER_NOTCHES = 20;
-const float ENCODER_CIRCUMFERENCE = 21.05; // Approximation
+const float WHEEL_CIRCUMFERENCE = 21.05; // Approximation
 
 void encoder_callback(uint gpio, uint32_t events) {
     // Encoder variables for distance and speed
@@ -38,7 +41,7 @@ void encoder_callback(uint gpio, uint32_t events) {
 
             // Increment the notch count by 1 and calculate distance travelled
             left_notch_count++;
-            float distance = (float)left_notch_count / ENCODER_NOTCHES * ENCODER_CIRCUMFERENCE;
+            float distance = (float)left_notch_count / ENCODER_NOTCHES * WHEEL_CIRCUMFERENCE;
 
             // Print the results
             printf("Left motor: Pulse width = %d us, Notch count = %d, Distance = %.2f cm\n", pulse_width, left_notch_count, distance);
@@ -50,7 +53,7 @@ void encoder_callback(uint gpio, uint32_t events) {
             right_last_edge_time = current_time;
 
             right_notch_count++;
-            float distance = (float)right_notch_count / ENCODER_NOTCHES * ENCODER_CIRCUMFERENCE;
+            float distance = (float)right_notch_count / ENCODER_NOTCHES * WHEEL_CIRCUMFERENCE;
 
             printf("Right motor: Pulse width = %d us, Notch count = %d, Distance = %.2f cm\n", pulse_width, right_notch_count, distance);
         }
@@ -59,6 +62,23 @@ void encoder_callback(uint gpio, uint32_t events) {
 
 int main() {
     stdio_init_all();
+
+    // motor init
+    motor_init();
+    setupPWM(0);
+    setupPWM(1);
+
+    // Set pin as high for left encoder
+    gpio_init(LEFT_VCC);
+    gpio_set_dir(LEFT_VCC, GPIO_OUT);
+    gpio_put(LEFT_VCC, 1);
+
+    // Set pin as high for right encoder
+    gpio_init(RIGHT_VCC);
+    gpio_set_dir(RIGHT_VCC, GPIO_OUT);
+    gpio_put(RIGHT_VCC, 1);
+
+    moveForward();
 
     // Set up the interrupt to trigger encoder_callback when rising edge is detected
     gpio_set_irq_enabled_with_callback(LEFT_ENCODER_PIN, GPIO_IRQ_EDGE_RISE, true, &encoder_callback);
