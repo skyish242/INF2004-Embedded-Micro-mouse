6b2cb5c1c6738984005b37bccaa10d32863e9929
Shauntayy <34932326+Shauntayy@users.noreply.github.com>
2023-11-30 22:45:10 +0800

    update comments

--- driver/ultrasonic/ultrasonic.c
+++ driver/ultrasonic/ultrasonic.c
@@ -1,5 +1,7 @@
 #include "ultrasonic.h"
 
+
+// Function to handle interrupts on the ECHO_PIN
 void echo_pin_handler(uint gpio, uint32_t events) {
     if (rising_edge) {
         start_time = get_absolute_time();
@@ -11,19 +13,21 @@ void echo_pin_handler(uint gpio, uint32_t events) {
     rising_edge = !rising_edge;
 }
 
+// Kalman filter function to smooth out noisy sensor readings
 double kalman(double U) {
-    static const double R = 40;
-    static const double H = 1;
-    static double Q = 10;
-    static double P = 0;
-    static double U_hat = 0;
-    static double K = 0;
+    static const double R = 40; // Measurement noise
+    static const double H = 1; // Measurement mapping
+    static double Q = 10; // Process noise
+    static double P = 0; // Estimate error covariance
+    static double U_hat = 0; // Estimated measurement
+    static double K = 0; // Kalman gain
     K = P * H / (H * P * H + R);
     U_hat += K * (U - H * U_hat);
     P = (1 - K * H) * P + Q;
     return U_hat;
 }
 
+// Setup function to initialize GPIO pins and set up interrupt handling
 void setup() {
     stdio_init_all();
     gpio_init(TRIG_PIN);
@@ -43,6 +47,7 @@ int main() {
     setupPWM(1);
 
     double filtered_distance_cm = 0; // Initialize the filtered distance
+    
     while (1) {
         gpio_put(TRIG_PIN, 1);
         sleep_us(10); // Send a 10us pulse on the TRIG pin
@@ -54,11 +59,11 @@ int main() {
 
         // Calculate the delay in milliseconds based on the maximum distance
         int delay_ms = (2 * max_distance_cm / speed_of_sound_cm_per_s) * 1000;
-        sleep_ms(delay_ms); // Adjust this based on your sensor's maximum range
+        sleep_ms(delay_ms); // Adjust based on sensor's maximum range
 
         // Calculate distance based on the timestamps
         int32_t duration_us = absolute_time_diff_us(start_time, end_time);
-        float distance_cm = duration_us / 58.0f; // Convert to centimeters (approximate)
+        float distance_cm = duration_us / 58.0f; // Convert to centimeters 
 
         // Use the Kalman filter to estimate the distance
         filtered_distance_cm = kalman(distance_cm);
